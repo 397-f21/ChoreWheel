d1f50bc1ca539003ae33f6d0699a0b85a5f54e35
Caspar Popova <71401509+cjpopova@users.noreply.github.com>
2021-10-04 19:39:04 -0500

    Implement checkboxes for multiple users

--- src/App.js
+++ src/App.js
@@ -7,41 +7,49 @@ const apartmentDB = {
       "taskIdCounter": 8,
       "tasks": {
         "idT0": {
+          "id" : "idT0",
           "title": "Cleaning Kitchen",
           "interval": 4,
           "daysRemaining": 2
         },
         "idT1": {
+          "id" : "idT1",
           "title": "Cleaning washroom",
           "interval": 3,
           "daysRemaining": 1
         },
         "idT2": {
+          "id" : "idT2",
           "title": "Checking mail",
           "interval": 3,
           "daysRemaining": 1
         },
         "idT3": {
+          "id" : "idT3",
           "title": "Taking trash out",
           "interval": 3,
           "daysRemaining": 1
         },
         "idT4": {
+          "id" : "idT4",
           "title": "Cleaning dishes",
           "interval": 3,
           "daysRemaining": 1
         },
         "idT5": {
+          "id" : "idT5",
           "title": "get groceries",
           "interval": 3,
           "daysRemaining": 1
         },
         "idT6": {
+          "id" : "idT6",
           "title": "Cleaning stovetop",
           "interval": 3,
           "daysRemaining": 1
         },
         "idT7": {
+          "id" : "idT7",
           "title": "vacuum floor",
           "interval": 3,
           "daysRemaining": 1
@@ -51,89 +59,84 @@ const apartmentDB = {
       "users": {
         "idU0": {
           "name": "sathu",
-          "tasks": [{
-            "task": "idT1",
-            "completed": true
-          },
-          {
-            "task": "idT0",
-            "completed": false
-          }]
+          "id" : "idU0",
+          "tasks": {
+            "idT1" : true,
+            "idT0" : true
+           }
         },
         "idU1": {
           "name": "Caspar",
-          "tasks": [{
-            "task": "idT0",
-            "completed": false
-          },
-          {
-            "task": "idT7",
-            "completed": false
+          "id" : "idU1",
+          "tasks": {
+            "idT0" : false,
+            "idT7" : false
           }
-          ]
         },
         "idU2": {
+          "id" : "idU2",
           "name": "Jun Li",
-          "tasks": [{
-            "task": "idT2",
-            "completed": true
-          }]
+          "tasks": {
+             "idT2" : true
+          }
         },
         "idU3": {
+          "id" : "idU3",
           "name": "Omar",
-          "tasks": [{
-            "task": "idT3",
-            "completed": true
-          }]
+          "tasks": {
+            "idT3" :  true
+          }
         },
         "idU4": {
+          "id" : "idU4",
           "name": "Spencer",
-          "tasks": [{
-            "task": "idT4",
-            "completed": true
-          }]
+          "tasks": {
+             "idT4" : true
+          }
         },
         "idU5": {
+          "id" : "idU5",
           "name": "Yifei",
-          "tasks": [{
-            "task": "idT5",
-            "completed": true
-          }]
+          "tasks": {
+            "idT5" : true
+          }
         },
       }
     }
   }
 }
 
-const Task = ({ task }) => (
-  <div className='card m-2 p-2 col-lg-8'>
+const Task = ({ task, updateTask }) => (
+  <div className='card m-2 p-2 col-lg-8' key={task.id}>
     <div className="d-flex justify-content-between align-items-center">
       <div className="card-body">
         <div className="card-title">{task.title}</div>
         <div className="card-text">{task.interval}</div>
       </div>
       <div /*className="form-switch"*/>
-        <input className="form-check-input" type="checkbox" checked={task.completed} value={task.completed} id="flexCheckDefault"/>
+        <input className="form-check-input" type="checkbox" defaultChecked={task.completed} value={task.completed} id="flexCheckDefault"
+        onChange={ updateTask(task.id)}/>
       </div>
     </div>
   </div>
 );
 
 
-const TaskList = ({ tasks }) => (
+const TaskList = ({ tasks, updateTask }) => (
   <div>
-    {Object.values(tasks).map( (task, idx) => <Task key={idx} task={task} />)}
+    {Object.values(tasks).map( (task, idx) => <Task key={idx} task={task} updateTask={updateTask} />)}
   </div>
 );
 
-const UserList = ({users, setUser, apt}) => (
+const UserList = ({users, setUser}) => (
   <div>
-    {Object.values(users).map((user, idx) => <UserButton user={user} setUser ={setUser}/>)}
+    {Object.values(users).map((user, idx) => <UserButton user={user} setUser ={setUser} key={idx}/>)}
   </div>
 )
 
 const UserButton = ({user, setUser}) => (
-  <button type="button" className="btn btn-primary m-2"> {user.name} </button>
+  <button type="button" className="btn btn-primary m-2"
+  onClick={() => setUser(user)}> {user.name} </button>
 )
 
 const getTask = (taskId, apt) => (
@@ -143,11 +146,14 @@ const getTask = (taskId, apt) => (
 function App() {
   const [apt, setApt] = useState(apartmentDB.apartments.idA0);
   const [user, setUser] = useState(apt.users.idU0);
+  const updateTask = (taskID) => {
+    apt.users[user.id].tasks[taskID] = apt.users[user.id].tasks[taskID];
+  }
 
   return (
     <div className='container' >
       <h1 > Hi {user.name}, your tasks are </h1>
-      <TaskList tasks={user.tasks.map( task => ({...getTask(task.task, apt), completed:task.completed}))} />
+      <TaskList tasks={Object.keys(user.tasks).map( taskID => ({...getTask(taskID, apt), completed:user.tasks[taskID]}))} updateTask={updateTask} />
       <UserList users={apt.users} setUser = {setUser}/>
     </div >
   );
