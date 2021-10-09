1c16f3d18b3f5578c1bbc11895f059d77be4161c
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-10-09 14:38:01 -0500

    Updated code formatting for clarity

--- src/ApartmentLogin.js
+++ src/ApartmentLogin.js
@@ -1,37 +1,44 @@
 import React from 'react';
 
 
-
-const ApartmentLoginButton = ({onFinish,aptKeys}) => (
-    <button type="button" className="btn btn-primary" 
-    onClick = {() => {
-      const input = document.querySelector("#apt_input")
-      if (aptKeys.includes(input.value)) {
-        onFinish(input.value)
-      }
-      else {
-        alert('This ID does not exist')
-      }
-      }}> 
+const ApartmentLoginButton = ({ onFinish, aptKeys }) => (
+  <button type="button" 
+      className="btn btn-primary" 
+      onClick = {
+        () => {
+          const input = document.querySelector("#apt_input")
+          if (aptKeys.includes(input.value)) {
+            onFinish(input.value)
+          } else {
+            alert('This ID does not exist')
+          }
+        }
+      }> 
     Log in to Apartment
   </button>
-
 );
 
-
-function ApartmentLogin ({onFinish,aptKeys})  {
-    return (
-      <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Apartment ID</label>
-          <input type="email" class="form-control" id="apt_input" aria-describedby="emailHelp" placeholder="Enter email"/>
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <ApartmentLoginButton onFinish={onFinish} aptKeys={aptKeys}/>
-      </form>
-    );
-  }
+function ApartmentLogin ({ onFinish, aptKeys }) {
+  return (
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">
+          Apartment ID
+        </label>
+        <input type="email" 
+            class="form-control" 
+            id="apt_input" 
+            aria-describedby="emailHelp" 
+            placeholder="Enter email" />
+        <small id="emailHelp" 
+            class="form-text text-muted">
+          We'll never share your email with anyone else.
+        </small>
+      </div>
+      <ApartmentLoginButton onFinish={onFinish} aptKeys={aptKeys}/>
+    </form>
+  );
+};
   
-
-  export default ApartmentLogin;
+export default ApartmentLogin;
    
--- src/App.js
+++ src/App.js
@@ -10,15 +10,24 @@ const Task = ({ task, updateTask }) => {
   currDate.setDate(currDate.getDate() + task.daysRemaining);
 
   return (
-  <div className={`card m-2 p-2 col-lg-8 mx-auto ${task.completed ? 'border-success bg-success bg-opacity-25': task.daysRemaining === 0 ? 'border-warning bg-warning bg-opacity-25' : 'border-dark'}`} style={{borderWidth: '4px'}} key={task.id}>
+  <div key={task.id} 
+      className={`card m-2 p-2 col-lg-8 mx-auto ${ task.completed ? 'border-success bg-success bg-opacity-25': task.daysRemaining === 0 ? 'border-warning bg-warning bg-opacity-25' : 'border-dark'}`} 
+      style={{borderWidth: '4px'}} >
     <div className="d-flex justify-content-between align-items-center">
-      <div className={'card-body'}>
-        <div className="card-title"><b>Task:</b> {task.title}</div>
-        <div className="card-text"><b>Due:</b> {currDate.toDateString()}</div>
+      <div className="card-body">
+        <div className="card-title">
+          <b>Task:</b> { task.title }
+        </div>
+        <div className="card-text">
+          <b>Due:</b> { currDate.toDateString() }
+        </div>
       </div>
       <div /*className="form-switch"*/>
-        <input className="form-check-input" type="checkbox" defaultChecked={task.completed} value={task.completed} id="flexCheckDefault"
-        onChange={ () => updateTask(task.id) }/>
+        <input type="checkbox"
+            className="form-check-input"  
+            defaultChecked={task.completed} 
+            value={task.completed} id="flexCheckDefault"
+            onChange={ () => updateTask(task.id) } />
       </div>
     </div>
   </div>
@@ -28,23 +37,32 @@ const Task = ({ task, updateTask }) => {
 
 const TaskList = ({ tasks, updateTask }) => (
   <div>
-    {Object.values(tasks).map( (task, idx) => <Task key={idx} task={task} updateTask={updateTask} />)}
+    {
+      Object.values(tasks).map((task, idx) => <Task key={idx} task={task} updateTask={updateTask} />)
+    }
   </div>
 );
 
-const UserList = ({currUser, users, setUser}) => (
+const UserList = ({ currUser, users, setUser }) => (
   <div className="container text-center">
-    {users.map((user, idx) => <UserButton user={user} selected={user.id === currUser}setUser={setUser} key={idx}/>)}
+    {
+      users.map((user, idx) => (
+        <UserButton key={idx}
+            user={user} 
+            selected={user.id === currUser}
+            setUser={setUser} />
+      ))
+    }
   </div>
   
 )
 
-const UserButton = ({user, selected, setUser}) => (
+const UserButton = ({ user, selected, setUser }) => (
   <button type="button" 
       className={`btn m-2 ${selected ? 'btn-primary' : 'btn-outline-primary'}`}
-      disabled={selected}
-      onClick={() => setUser(user.id)}> 
-    {user.name} 
+      disabled={ selected }
+      onClick={ () => setUser(user.id) }> 
+    { user.name } 
   </button>
 )
 
@@ -52,52 +70,63 @@ const getTask = (taskId, apt) => (
   apt.tasks[taskId]
 );
 
-const ApartmentTaskList = ({users, tasks, updateTask}) => (
+const ApartmentTaskList = ({ users, tasks, updateTask }) => (
   <div>
-    {Object.values(users).map( (user, idx) => 
-    Object.keys(user.tasks).map( (taskId, taskIdx) => 
-    (<Task key={`${idx}-${taskIdx}`} task={{... tasks[taskId], completed:user.tasks[taskId]}} updateTask={updateTask} />)))}
+    {
+      Object.values(users).map( (user, idx) => (
+        Object.keys(user.tasks).map( (taskId, taskIdx) => (
+          <Task key={`${idx}-${taskIdx}`} 
+              task={{...tasks[taskId], completed:user.tasks[taskId]}} 
+              updateTask={updateTask} />
+        ))
+      ))
+    }
   </div>
 )
 
 function App() {
   const [aptId, setApt] = useState();
-  const [data, loading, error] = useData(`/apartments`);
   const [user, setUser] = useState('');  
-  if (error) return <h1>{error}</h1>;
+  const [data, loading, error] = useData(`/apartments`);
+  if (error) return <h1>{ error }</h1>;
   if (loading) return <h1>Loading the tasks...</h1>;
-  if (!aptId) return <ApartmentLogin onFinish = {setApt} aptKeys={Object.keys(data)}/>;
+
+  if (!aptId) return <ApartmentLogin onFinish={ setApt } aptKeys={ Object.keys(data) } />;
 
   const apt = data[aptId];
   const userData = apt.users[user];
   const updateTask = (taskID) => {
     setData(`/apartments/${aptId}/users/${user}/tasks/${taskID}`, !userData.tasks[taskID])
-
-  }
+  };
 
   return (
     <div>
       <div className='navbar navbar-light bg-light'>
         <div className='container-fluid'>
           <span className='navbar-brand justify-content-bottom'>
-            <FaDharmachakra size="1.7em" style={{color: '#d4af37', marginTop: '-.4em'}}/>
-            <span className="h2 fw-bold"> ChoreWheel </span>
+            <FaDharmachakra size="1.7em" style={{color: '#d4af37', marginTop: '-.4em'}} />
+            <span className="h2 fw-bold"> 
+              ChoreWheel 
+            </span>
           </span>
         </div>
       </div>
 
       <div className='container pt-2' >
         { user ?  (
-            <TaskList 
-            tasks={Object.keys(userData.tasks).map( taskID => ({...getTask(taskID, apt), completed:userData.tasks[taskID]}))} 
-            updateTask={updateTask} />
+          <TaskList 
+            tasks={ Object.keys(userData.tasks).map(taskId => ({...getTask(taskId, apt), completed:userData.tasks[taskId]})) } 
+            updateTask={ updateTask}  />
         ) : (
-          <ApartmentTaskList users={apt.users} tasks={apt.tasks} updateTask={updateTask}/>
+          <ApartmentTaskList 
+            users={ apt.users } 
+            tasks={ apt.tasks }
+            updateTask={ updateTask } />
         ) }
         <UserList 
-            currUser={user} 
-            users={[{id:'', name:'Full Apartment'}, ...Object.values(apt.users)]} 
-            setUser={setUser} />
+            currUser={ user } 
+            users={ [{id:'', name:'Full Apartment'}, ...Object.values(apt.users)] } 
+            setUser={ setUser } />
       </div >
     </div>
     
