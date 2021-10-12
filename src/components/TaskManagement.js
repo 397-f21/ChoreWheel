e4d632a44daf69ebfe1d0b79286e835258d9e1dd
Caspar Popova <71401509+cjpopova@users.noreply.github.com>
2021-10-11 21:18:04 -0500

    Draft new task form

---
+++ src/components/TaskManagement.js
@@ -0,0 +1,35 @@
+const AddTask = () => (
+    <div className="container">
+        <form>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Task</label>
+                <input class="form-control" id="exampleFormControlInput1" placeholder="Clean living room" />
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Assign to:</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                    <option>user 1</option>
+                    <option>user 2</option>
+                    <option>user 3</option>
+                    <option>user 4</option>
+                    <option>user 5</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlSelect2">Daily interval</label>
+                <select multiple class="form-control" id="exampleFormControlSelect2">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+            </div>
+
+            <button type="submit" class="btn btn-primary m-2">Add task</button>
+            <button type="submit" class="btn btn-secondary m-2">Cancel</button>
+        </form>
+    </div>
+)
+
+export default AddTask
