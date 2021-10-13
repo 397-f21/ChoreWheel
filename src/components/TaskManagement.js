085f41b3c6e2804d55c8ed2ecaba461231094a24
OmarKhatib01 <osokhatib1@gmail.com>
2021-10-12 21:12:16 -0500

    validation

--- src/components/TaskManagement.js
+++ src/components/TaskManagement.js
@@ -10,8 +10,39 @@ const validateForm = () => {
     const taskName = document.querySelector('#taskName').value;
     const assignedUser = document.querySelector('#assignUser').value;
     const interval = document.querySelector('#interval').value;
-    const dueDate = document.querySelector('#dueDate').value;
-    const date = new Date(dueDate.slice(0,4), Number(dueDate.slice(5,7))-1, dueDate.slice(8)); console.log(date);
+    const dueDateVal = document.querySelector('#dueDate').value;
+    
+    if (taskName === ''){
+      alert('Please enter a name for the task')
+      return
+    }
+    if (assignedUser === ''){
+      alert('Please choose a user')
+      return
+    }
+    if (dueDateVal === ''){
+      alert('Please pick a valid date')
+      return
+    }
+    const dueDate = new Date(dueDateVal.slice(0,4), Number(dueDateVal.slice(5,7))-1, dueDateVal.slice(8));
+    const currDate = new Date(Date.now());
+    dueDate.setHours(0, 0, 0, 0)
+    currDate.setHours(0,0, 0, 0)
+    if (dueDate <  currDate){
+      alert('Please pick a valid date')
+      return
+    }
+    if (interval === ''){
+      alert('Please enter positive whole number')
+      return
+    }
+
+    const intervalNum = Number(interval)
+
+    if (intervalNum < 1 || !Number.isInteger(intervalNum) ){
+      alert('Please enter positive whole number')
+    }
+    
 
 
 }
@@ -30,7 +61,7 @@ const AddTask = ({show, handleClose,users}) =>  (
         <div className="form-group p-2">
           <label htmlFor="assignUser">Assign to:</label>
           <select className="form-select" id="assignUser">
-            <option disabled>Choose a user</option>
+            <option value="" disabled selected hidden>Choose a user</option>
             {users.map(user => <option value={user.id} key={user.id}> {user.name} </option>)}
           </select>
         </div>
