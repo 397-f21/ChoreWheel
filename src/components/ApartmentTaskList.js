eaa9916ddb7d287be6327afc1e403af6e7dabb42
Enlime <632476407@qq.com>
2021-10-17 15:49:14 -0500

    sorting the task for full apartment

--- src/components/ApartmentTaskList.js
+++ src/components/ApartmentTaskList.js
@@ -1,20 +1,23 @@
 import Task from "./Task"
 
-const ApartmentTaskList = ({ aptId, users, tasks}) => (
+const ApartmentTaskList = ({ aptId, users, tasks}) => {
+  const flatTasks = Object.values(users).map((user, idx) => 
+  (Object.keys(user.tasks).map((taskId) => ({...tasks[taskId], completed:user.tasks[taskId], user:user})))).flat().sort((task1, task2) => 
+  task1.daysRemaining - task2.daysRemaining)
+  return (
   <div>
     {
-      Object.values(users).map( (user, idx) => (
-        user.tasks && Object.keys(user.tasks).map( (taskId, taskIdx) => (
-          <Task key={ `${idx}-${taskIdx}` } 
+          flatTasks.map((task,idx) => (
+          <Task key={ idx } 
               aptId={ aptId }
-              userId={ user.id }
-              task={ {...tasks[taskId], completed:user.tasks[taskId]} } >
-            { user.name }
+              userId={ task.user.id }
+              task={ task } >
+            { task.user.name }
           </Task>
         ))
-      ))
     }
   </div>
-);
+  )
+};
   
 export default ApartmentTaskList;
